84d7e105091177c9f5ec9eef043e50cde823475a
lindomash001 <lindomash001@gmail.com>
2025-01-08 20:35:59 +0200

    firewall checker working

--- ser_analysis.sh
+++ ser_analysis.sh
@@ -1,9 +1,50 @@
+
+
+
+
+
+
 update_system() {
+    echo "===== Updating System =====" >> "$output_file"
     if command -v apt &> /dev/null; then
         echo "Updating using apt..." >> "$output_file"
+        sudo apt update && sudo apt upgrade -y >> "$output_file"
+    elif command -v yum &> /dev/null; then
+        echo "Updating using yum..." >> "$output_file"
+        sudo yum update -y >> "$output_file"
+    else
+        echo "Unsupported package manager. Please update manually." >> "$output_file"
     fi
     echo >> "$output_file"
 }
+
+
+check_firewall() {
+    echo "===== Checking Firewall  =====" >> "$output_file"
+    if command -v ufw &> /dev/null; then
+        ufw_status=$(sudo ufw status | grep "Status: active")
+        if [ -n "$ufw_status" ]; then
+            echo "Firewall is active." >> "$output_file"
+        else
+            echo "Firewall is not active." >> "$output_file"
+        fi
+    else
+        echo "UFW is not installed. Please install it first." >> "$output_file"
+    fi
+    echo >> "$output_file"
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 update_system
